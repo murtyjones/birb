e4437c085962a9126f6f99c75770d3a2e0b27cb2
Marty Jones <murtin.jones@gmail.com>
2019-04-06 20:30:41 -0400

    cleanup

--- crates/api/src/lib/mongo.rs
+++ crates/api/src/lib/mongo.rs
@@ -5,7 +5,6 @@ use mongodb::ordered::OrderedDocument;
 
 pub struct Mongo;
 
-#[cfg(not(test))]
 impl Mongo {
     pub fn find_one(
         conn: &Database,
@@ -13,13 +12,5 @@ impl Mongo {
         filter: Option<OrderedDocument>,
     ) -> Option<OrderedDocument> {
         conn.collection(collection).find_one(filter, None).unwrap()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn find_one_unwraps() {
-        assert_eq!(1, 1)
     }
 }
