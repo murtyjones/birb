4ecae25ed9443eecfdcc56fac36a98cd405f5c93
Marty Jones <murtin.jones@gmail.com>
2019-04-07 16:09:29 -0400

    Just one test

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -46,27 +46,27 @@ mod test {
     use super::rocket;
     use rocket::http::{ContentType, Status};
     use rocket::local::Client;
-    #[test]
-    fn bad_get() {
-        let client = Client::new(rocket()).unwrap();
-        let res = client
-            .get("/doesnotexist")
-            .header(ContentType::JSON)
-            .dispatch();
-        assert_eq!(res.status(), Status::NotFound);
-    }
+    //    #[test]
+    //    fn bad_get() {
+    //        let client = Client::new(rocket()).unwrap();
+    //        let res = client
+    //            .get("/doesnotexist")
+    //            .header(ContentType::JSON)
+    //            .dispatch();
+    //        assert_eq!(res.status(), Status::NotFound);
+    //    }
 
     #[test]
     fn get_tsla() {
         let client = Client::new(rocket()).unwrap();
-        let res = client
+        let mut res = client
             .get("/company/0001318605")
             .header(ContentType::JSON)
             .dispatch();
-        //        let body = res.body_string().unwrap();
+        let body = res.body_string().unwrap();
         assert_eq!(res.status(), Status::Ok);
-        //        assert!(body.contains("TESLA MOTORS INC"));
-        //        assert!(body.contains("0001318605"));
+        assert!(body.contains("TESLA MOTORS INC"));
+        assert!(body.contains("0001318605"));
     }
 
 }
