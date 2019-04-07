103751ea24acf8b8ba1a2a5398ea15a151dc5acb
Marty Jones <murtin.jones@gmail.com>
2019-04-07 08:19:05 -0400

    Silence mismatch type warning in PHPStorm

--- crates/api/src/handlers/company.rs
+++ crates/api/src/handlers/company.rs
@@ -7,7 +7,7 @@ use rocket_contrib::json::{Json, JsonValue};
 
 #[get("/company/<cik>")]
 pub fn get(conn: DbConnection, cik: String) -> Json<JsonValue> {
-    let doc = models::company::Model::find_one_by_cik(&conn, cik.to_owned());
+    let doc = models::company::Model::find_one_by_cik(&conn.0, cik.to_owned());
     //    let _company = bson::Bson::Document(doc);
     let company = bson::from_bson::<meta::company::GetResponse>(bson::Bson::Document(doc));
     Json(json!({
