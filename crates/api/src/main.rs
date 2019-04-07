fc6d0187b2d9f3220bd2dc33159b10a337556234
Marty Jones <murtin.jones@gmail.com>
2019-04-07 15:26:48 -0400

    Reenable tesla test

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -56,17 +56,17 @@ mod test {
         assert_eq!(res.status(), Status::NotFound);
     }
 
-    //    #[test]
-    //    fn get_tsla() {
-    //        let client = Client::new(rocket()).unwrap();
-    //        let mut res = client
-    //            .get("/company/0001318605")
-    //            .header(ContentType::JSON)
-    //            .dispatch();
-    //        assert_eq!(res.status(), Status::Ok);
-    //        let body = res.body_string().unwrap();
-    //        assert!(body.contains("TESLA MOTORS INC"));
-    //        assert!(body.contains("0001318605"));
-    //    }
+    #[test]
+    fn get_tsla() {
+        let client = Client::new(rocket()).unwrap();
+        let mut res = client
+            .get("/company/0001318605")
+            .header(ContentType::JSON)
+            .dispatch();
+        let body = res.body_string().unwrap();
+        assert_eq!(res.status(), Status::Ok);
+        assert!(body.contains("TESLA MOTORS INC"));
+        assert!(body.contains("0001318605"));
+    }
 
 }
