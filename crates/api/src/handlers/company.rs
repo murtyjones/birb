79b5f377630aa463d2ecd99f58f387d807a5eb9e
Marty Jones <murtin.jones@gmail.com>
2019-04-07 16:52:11 -0400

    debug company

--- crates/api/src/handlers/company.rs
+++ crates/api/src/handlers/company.rs
@@ -8,9 +8,13 @@ use rocket_contrib::json::{Json, JsonValue};
 
 #[get("/company/<cik>")]
 pub fn get(conn: DbConnection, cik: String) -> Json<JsonValue> {
+    dbg!("get from db");
     let doc = models::company::Model::find_one_by_cik(&conn.0, cik.to_owned());
-    //    let _company = bson::Bson::Document(doc);
+    dbg!("resp from db");
+    dbg!(&doc);
     let company = bson::from_bson::<meta::company::GetResponse>(bson::Bson::Document(doc));
+    dbg!("converted");
+    dbg!(&company);
     Json(json!(GetResponse {
         status: ResponseStatuses::OK,
         object_type: ObjectTypes::Object,
