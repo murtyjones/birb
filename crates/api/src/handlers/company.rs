e54a07a8eb808423ec69f6476d8b4e3ac4948f4d
Marty Jones <murtin.jones@gmail.com>
2019-04-07 16:56:58 -0400

    expect valid rocket instance

--- crates/api/src/handlers/company.rs
+++ crates/api/src/handlers/company.rs
@@ -8,13 +8,8 @@ use rocket_contrib::json::{Json, JsonValue};
 
 #[get("/company/<cik>")]
 pub fn get(conn: DbConnection, cik: String) -> Json<JsonValue> {
-    dbg!("get from db");
     let doc = models::company::Model::find_one_by_cik(&conn.0, cik.to_owned());
-    dbg!("resp from db");
-    dbg!(&doc);
     let company = bson::from_bson::<meta::company::GetResponse>(bson::Bson::Document(doc));
-    dbg!("converted");
-    dbg!(&company);
     Json(json!(GetResponse {
         status: ResponseStatuses::OK,
         object_type: ObjectTypes::Object,
--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -48,7 +48,7 @@ mod test {
     use rocket::local::Client;
     #[test]
     fn bad_get() {
-        let client = Client::new(rocket()).unwrap();
+        let client = Client::new(rocket()).expect("valid rocket instance");
         let res = client
             .get("/doesnotexist")
             .header(ContentType::JSON)
@@ -58,7 +58,7 @@ mod test {
 
     #[test]
     fn get_tsla() {
-        let client = Client::new(rocket()).unwrap();
+        let client = Client::new(rocket()).expect("valid rocket instance");
         let mut res = client
             .get("/company/0001318605")
             .header(ContentType::JSON)
