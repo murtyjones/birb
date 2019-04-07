164555960e1ee4aaa263255dabbe3800c579c45a
Marty Jones <murtin.jones@gmail.com>
2019-04-07 16:26:30 -0400

    Try a different way of including test module

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -19,6 +19,7 @@ mod lib;
 mod meta;
 mod mocks;
 mod models;
+#[cfg(test)] mod test;
 
 #[database("mongo_datastore")]
 pub struct DbConnection(mongodb::db::Database);
@@ -39,34 +40,4 @@ fn rocket() -> rocket::Rocket {
 
 fn main() {
     rocket().launch();
-}
-
-#[cfg(test)]
-mod test {
-    use super::rocket;
-    use rocket::http::{ContentType, Status};
-    use rocket::local::Client;
-    #[test]
-    fn bad_get() {
-        let client = Client::new(rocket()).unwrap();
-        let res = client
-            .get("/doesnotexist")
-            .header(ContentType::JSON)
-            .dispatch();
-        assert_eq!(res.status(), Status::NotFound);
-    }
-
-    #[test]
-    fn get_tsla() {
-        let client = Client::new(rocket()).unwrap();
-        let mut res = client
-            .get("/company/0001318605")
-            .header(ContentType::JSON)
-            .dispatch();
-        let body = res.body_string().unwrap();
-        assert_eq!(res.status(), Status::Ok);
-        assert!(body.contains("TESLA MOTORS INC"));
-        assert!(body.contains("0001318605"));
-    }
-
 }
