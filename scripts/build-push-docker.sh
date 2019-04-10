05031d7579f9635914f3674e7f0b93ec54b2f2c5
Marty Jones <murtin.jones@gmail.com>
2019-04-09 20:59:57 -0400

    Formatting tweaks

--- scripts/build-push-docker.sh
+++ scripts/build-push-docker.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
+# Go to project root
+cd $(git rev-parse --show-toplevel)
+
+# Set variables to use in the tagging process
 NAME=api
 VERSION=$(shell git rev-parse HEAD)
 SEMVER_VERSION=$(shell grep version Cargo.toml | awk -F"\"" '{print $$2}' | head -n 1)
 REPO=birb
 
-# Go to project root
-cd $(git rev-parse --show-toplevel)
 
 # docker login to ECR registry
-aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email | sh &&
+aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email | sh \
+&&
 
 # Build docker image for production and push to ECR
-docker build -t $(REPO)/$(NAME):$(VERSION) -t $(REPO)/$(NAME):latest -f ./out/Dockerfile-prod . &&
+docker build -t $REPO/$NAME:$VERSION -t $REPO/$NAME:latest -f ./out/Dockerfile-prod . \
+&&
 
 # Tag auth
-docker tag $(REPO)/$(NAME):latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$(REPO)/$(NAME):latest &&
+docker tag $REPO/$NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$REPO/$NAME:latest \
+&&
 
 # Push to ECR
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/auth:latest \
-
 # &&
 
 # Force cluster to restart with new image
