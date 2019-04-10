7e37a19bbe0cae428b6a42444c0629bbc1176a7e
Marty Jones <murtin.jones@gmail.com>
2019-04-09 22:24:41 -0400

    fix legacy name

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -19,11 +19,11 @@ aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email | sh
 # Build docker image for production and push to ECR
 docker build -t $REPO/$NAME:$VERSION -t $REPO/$NAME:latest -f Dockerfile-prod ./out
 
-# Tag auth
+# Tag image
 docker tag $REPO/$NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$NAME:latest
 
 # Push to ECR
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/auth:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$NAME:latest
 
 # Force cluster to restart with new image
 # aws ecs update-service --cluster cluster-name-here --service service-name-here --force-new-deployment
