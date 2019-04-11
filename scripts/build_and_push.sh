061518a7b43f1e7c6b8263a43aa7d73f0cd3a1dd
Marty Jones <murtin.jones@gmail.com>
2019-04-10 21:26:03 -0400

    service name

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -7,23 +7,22 @@ set -e
 cd $(git rev-parse --show-toplevel)
 
 # Set variables to use in the tagging process
-NAME=birb_api
+NAME_UNDER=birb_api
+NAME_HYPHEN=birb-api
 VERSION=$(git rev-parse HEAD)
-SEMVER_VERSION=$(grep version Cargo.toml | awk -F"\"" '{print $$2}' | head -n 1)
 REPO=murtyjones
-
 
 # docker login to ECR registry
 aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email | sh
 
 # Build docker image for production and push to ECR
-docker build -t $REPO/$NAME:$VERSION -t $REPO/$NAME:latest -f ./out/Dockerfile-prod ./out
+docker build -t $REPO/$NAME_UNDER:$VERSION -t $REPO/$NAME_UNDER:latest -f ./out/Dockerfile-prod ./out
 
 # Tag image
-docker tag $REPO/$NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$NAME:latest
+docker tag $REPO/$NAME_UNDER:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$NAME_UNDER:latest
 
 # Push to ECR
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$NAME:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$NAME_UNDER:latest
 
 # Force cluster to restart with new image
-aws ecs update-service --cluster $NAME-cluster --service $NAME-service --force-new-deployment
+aws ecs update-service --cluster $NAME_UNDER-cluster --service $NAME_HYPHEN-service --force-new-deployment
