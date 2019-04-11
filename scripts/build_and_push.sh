547986ae00e28fb6beca63e0f7362bf7d5d2b3c6
Marty Jones <murtin.jones@gmail.com>
2019-04-10 21:08:56 -0400

    use underscored where needed

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -8,6 +8,7 @@ cd $(git rev-parse --show-toplevel)
 
 # Set variables to use in the tagging process
 NAME=birb-api
+NAME_UNDERSCORED=birb-api
 VERSION=$(git rev-parse HEAD)
 SEMVER_VERSION=$(grep version Cargo.toml | awk -F"\"" '{print $$2}' | head -n 1)
 REPO=murtyjones
@@ -17,13 +18,13 @@ REPO=murtyjones
 aws ecr get-login --region $AWS_DEFAULT_REGION --no-include-email | sh
 
 # Build docker image for production and push to ECR
-docker build -t $REPO/$NAME:$VERSION -t $REPO/$NAME:latest -f ./out/Dockerfile-prod ./out
+docker build -t $REPO/$NAME_UNDERSCORED:$VERSION -t $REPO/$NAME_UNDERSCORED:latest -f ./out/Dockerfile-prod ./out
 
 # Tag image
-docker tag $REPO/$NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$NAME:latest
+docker tag $REPO/$NAME_UNDERSCORED:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$NAME_UNDERSCORED:latest
 
 # Push to ECR
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$NAME:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$NAME_UNDERSCORED:latest
 
 # Force cluster to restart with new image
 aws ecs update-service --cluster $NAME-cluster --service $NAME-service --force-new-deployment
