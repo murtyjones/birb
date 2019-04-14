f2cb8f7dcd02f6a8091581a43bb4495dd2449f05
Marty Jones <murtin.jones@gmail.com>
2019-04-14 15:08:37 -0400

    mock is_active

--- crates/filer-status/src/lib.rs
+++ crates/filer-status/src/lib.rs
@@ -1,36 +1,64 @@
 //! Returns information about a given filer's status. Intended
 //! to be as static as possible - it does make HTTP requests, but
 //! should not care about the DB.
-
+#![feature(proc_macro)]
 #![deny(missing_docs)]
-#[cfg(test)] extern crate mockers_derive;
-#[cfg(test)] use mockers_derive::mocked;
+extern crate mockers;
+extern crate mockers_derive;
+use mockers_derive::mocked;
 
+extern crate api_lib;
 extern crate bson;
 
-/// A filer's status
-#[cfg_attr(test, mocked)]
-pub trait Status {
-    /// Tells whether or not the filer is actively submitting filings
-    fn is_active(&mut self, cik: String) -> bool;
+use api_lib::models::filer::Model as Filer;
+
+/// Filing status of the filer
+#[mocked]
+pub trait FilingStatus {
+    /// Is the filer active in filing with the SEC?
+    fn is_active(&self) -> bool;
+    /// Gets the doc from sec.gov
+    fn get_10q_doc(&self) -> String;
 }
 
-/// Get the status for a filer given the cik
-pub fn get_filer_status(cik: String, cond: &mut Status) -> bool {
-    cond.is_active(cik)
+/// Implements the status retrieval for the Filer model
+impl FilingStatus for Filer {
+    fn is_active(&self) -> bool {
+        self.get_10q_doc();
+        true
+    }
+
+    fn get_10q_doc(&self) -> String {
+        self.cik.clone()
+    }
+}
+
+/// Wow!
+pub fn get_filer_status(f: &mut FilingStatus) -> bool {
+    f.is_active()
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     use mockers::Scenario;
+
     #[test]
-    fn test_get_status() {
+    fn test_get_filer_status() {
+        // Arrange
         let cik = String::from("0000000000");
+        let mut names = vec![];
+        names.push(bson::to_bson("marty").unwrap());
+        names.push(bson::to_bson("martin").unwrap());
         let scenario = Scenario::new();
-        let mut cond = scenario.create_mock_for::<Status>();
-        scenario.expect(cond.is_active_call(cik.clone()).and_return(true));
-        let r = get_filer_status(cik, &mut cond);
-        assert_eq!(r, true);
+        let mut mock = scenario.create_mock_for::<FilingStatus>();
+        let f = Filer { cik, names };
+
+        // Assert
+        scenario.expect(mock.is_active_call().and_return(false));
+
+        // Act
+        let r = get_filer_status(&mut mock);
+        assert_eq!(r, false)
     }
 }
