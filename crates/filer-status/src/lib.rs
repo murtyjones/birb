fbfc038d2a5026836b3b21deadac991c4299e974
Marty Jones <murtin.jones@gmail.com>
2019-04-16 06:19:38 -0400

    Mock out service call to get 10q listings

--- crates/filer-status/src/lib.rs
+++ crates/filer-status/src/lib.rs
@@ -11,12 +11,20 @@ extern crate reqwest;
 
 use api_lib::models::filer::Model as Filer;
 
+#[cfg(test)] use std::fs;
+#[cfg(test)] use std::path::Path;
+
+#[cfg(test)]
+const INACTIVE_FILER_CIK: &'static str = "0000948605"; // Kenneth Sawyer
+#[cfg(test)]
+const ACTIVE_FILER_CIK: &'static str = "0001318605"; // Tesla, Inc.
+
 /// Filing status of the filer
 pub trait FilingStatus {
     /// Is the filer active in filing with the SEC?
     fn is_active(&self) -> bool;
     /// Gets the doc from sec.gov
-    fn get_10q_doc(&self) -> Result<(), Box<std::error::Error>>;
+    fn get_10q_doc(&self) -> Result<String, Box<std::error::Error>>;
 }
 
 /// Implements the status retrieval for the Filer model
@@ -26,23 +34,25 @@ impl FilingStatus for Filer {
     }
 
     #[cfg(not(test))] // TODO use failure library instead of Box<...>
-    fn get_10q_doc(&self) -> Result<(), Box<std::error::Error>> {
-        let url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001318605&type=10-Q&dateb=&owner=include&count=40";
-        let resp: String = reqwest::get(url)?.text()?;
-        Ok(())
+    fn get_10q_doc(&self) -> Result<String, Box<std::error::Error>> {
+        let url: &str = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001318605&type=10-Q&dateb=&owner=include&count=40";
+        let html: String = reqwest::get(url)?.text()?;
+        Ok(html)
     }
 
     #[cfg(test)] // TODO use failure library instead of Box<...>
-    fn get_10q_doc(&self) -> Result<(), Box<std::error::Error>> {
-        let url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0000948605&type=10-Q&dateb=&owner=include&count=40";
-        let resp: String = reqwest::get(url)?.text()?;
-        println!("response body: {}", resp);
-        std::fs::write(
-            "/Users/murtyjones/birb/seed-data/unit-test/kenneth-sawyer-10q-listings",
-            resp,
-        )
-        .expect("Unable to write file");
-        Ok(())
+    fn get_10q_doc(&self) -> Result<String, Box<std::error::Error>> {
+        let mut filer_mock_html_path: &'static str = "";
+        if self.cik == INACTIVE_FILER_CIK {
+            filer_mock_html_path = "../../seed-data/unit-test/kenneth-sawyer-10q-listings";
+        } else if self.cik == ACTIVE_FILER_CIK {
+            filer_mock_html_path = "../../seed-data/unit-test/tsla-10q-listings";
+        }
+        // TODO: This path is relative to birb/crates/filer-status. Not sure whether it
+        // holds up at compile time but I guess since this is a test it's okay.
+        let path: &Path = Path::new(filer_mock_html_path);
+        let html: String = fs::read_to_string(path)?;
+        Ok(html)
     }
 }
 
@@ -67,12 +77,12 @@ mod test {
     }
 
     #[test]
-    fn test_get_10q() {
+    fn test_get_10q_listings_inactive_filer() {
         // Arrange
-        let cik = String::from("0000000000");
+        let cik = String::from(INACTIVE_FILER_CIK);
         let mut names = vec![];
-        names.push(bson::to_bson("marty").unwrap());
-        names.push(bson::to_bson("martin").unwrap());
+        names.push(bson::to_bson("ken sawyer").unwrap());
+        names.push(bson::to_bson("kenneth").unwrap());
         let f = Filer { cik, names };
 
         // Assert
