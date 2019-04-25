bdf5f2e8478208c93cf09c4f6a9a754a96a60019
Marty Jones <murtin.jones@gmail.com>
2019-04-25 08:19:56 -0400

    Dev dependencies

---
+++ scripts/install_dev_dependencies.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Exit immediately if any step errors:
+set -e
+
+# Go to project root
+cd $(git rev-parse --show-toplevel)
+
+
+# Install Postgres
+brew install postgres
+# Initialize postgres DB
+initdb /usr/local/var/postgres
+# Create postgres user
+/usr/local/opt/postgres/bin/createuser -s postgres which will just use the latest version.
+# Start postgres server manually
+pg_ctl -D /usr/local/var/postgres start
+
+# Start postgres at launch
+mkdir -p ~/Library/LaunchAgents
+ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+
+# Install PGTap
+pushd ~
+git clone https://github.com/theory/pgtap.git
+cd pgtap
+make
+make installcheck
+make install
+popd
+
+# Install Rust Nighhly
+# TODO
