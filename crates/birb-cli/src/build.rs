b576bac73e9d41c7d9ba0b827c0115061631b3c9
Marty Jones <murtin.jones@gmail.com>
2019-05-06 19:03:14 -0400

    rust formatting

--- crates/birb-cli/src/build.rs
+++ crates/birb-cli/src/build.rs
@@ -16,41 +16,47 @@ fn copy_artifacts(package: &str) {
     run_str_in_bash("mkdir out").unwrap();
     run_str_in_bash("cp ./crates/api/Dockerfile-prod out").unwrap();
     run_str_in_bash(
-        format!("cp ./target/x86_64-unknown-linux-musl/release/{} out", package).as_str()
-    ).unwrap();
+        format!(
+            "cp ./target/x86_64-unknown-linux-musl/release/{} out",
+            package
+        )
+        .as_str(),
+    )
+    .unwrap();
 }
 
 fn build_binary(package: &str) {
-    let build_command = format!("
+    let build_command = format!(
+        "
         # get base image
         docker pull clux/muslrust:nightly
 
         # build binary
         make build-release package={}
-    ", package);
+    ",
+        package
+    );
     run_str_in_bash(build_command.as_str()).unwrap();
 }
 
 fn zip_artifacts() {
-    run_str_in_bash(
-        "zip -j out.zip out/*"
-    ).unwrap();
+    run_str_in_bash("zip -j out.zip out/*").unwrap();
 }
 
 impl Subcommand for Build {
     fn run(&self) -> Result<(), failure::Error> {
         match self {
-            Build::Api=> {
+            Build::Api => {
                 build_binary("api");
                 copy_artifacts("api_bin");
                 Ok(())
-            },
+            }
             Build::Edgar => {
                 build_binary("edgar-worker");
                 copy_artifacts("bootstrap");
                 zip_artifacts();
                 Ok(())
-            },
+            }
         }
     }
 }
