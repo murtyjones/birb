2c225eb337a700d48e1a138b41627898d7306d7d
Marty Jones <murtin.jones@gmail.com>
2019-05-12 21:25:46 -0400

    openssl_probe

--- crates/edgar-worker/src/main.rs
+++ crates/edgar-worker/src/main.rs
@@ -1,4 +1,5 @@
 extern crate api_lib;
+extern crate openssl_probe;
 use std::env;
 
 use std::sync::mpsc::channel;
@@ -14,6 +15,7 @@ use postgres::{Connection, TlsMode};
 
 /// Find a filer with no status and update it.
 pub fn main() -> () {
+    openssl_probe::init_ssl_cert_env_vars();
     // Only make requests to the SEC every 5 seconds for now,
     // to be on the safe side.
     const SECONDS_DELAY: u16 = 5;
