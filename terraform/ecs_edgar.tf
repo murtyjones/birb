b5d68a34f5c9fb038ac8cb900a5834df323e9ebe
Marty Jones <murtin.jones@gmail.com>
2019-05-14 08:01:40 -0400

    decrease instance size

--- terraform/ecs_edgar.tf
+++ terraform/ecs_edgar.tf
@@ -25,7 +25,7 @@ data "template_file" "birb_edgar_worker_app" {
 resource "aws_launch_configuration" "ecs-launch-configuration" {
   name                 = "ecs-launch-configuration"
   image_id             = "ami-0bc08634af113cccb"
-  instance_type        = "t3.medium"
+  instance_type        = "t3.small"
   iam_instance_profile = "${aws_iam_instance_profile.ecs-instance-profile.id}"
   security_groups = [
     "${aws_security_group.birb-edgar.id}"
