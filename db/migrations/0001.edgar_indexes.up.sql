a35f15e33d33a580a4bb0b900a6c67e2d3237da9
Marty Jones <murtin.jones@gmail.com>
2019-05-17 09:35:50 -0400

    status enum

--- db/migrations/0001.edgar_indexes.up.sql
+++ db/migrations/0001.edgar_indexes.up.sql
@@ -1,3 +1,4 @@
+-- Used for updated_at field auto-update
 CREATE OR REPLACE FUNCTION trigger_set_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -6,11 +7,15 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- the status of a given edgar index
+CREATE TYPE index_status AS ENUM ('PROCESSED', 'FAILED');
+
 CREATE TABLE IF NOT EXISTS edgar_indexes (
   id SERIAL NOT NULL PRIMARY KEY
   ,index_name VARCHAR (30) NOT NULL
   ,index_year INTEGER
   ,index_quarter INTEGER
+  ,status index_status DEFAULT NULL
   ,created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
   ,updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
