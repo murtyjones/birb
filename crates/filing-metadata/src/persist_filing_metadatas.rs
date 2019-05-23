0dc8d75350122bbad58402702735fa207337117e
Marty Jones <murtin.jones@gmail.com>
2019-05-23 06:40:08 -0400

    filing statement upsertion

--- crates/filing-metadata/src/persist_filing_metadatas.rs
+++ crates/filing-metadata/src/persist_filing_metadatas.rs
@@ -8,7 +8,7 @@ use crate::time_periods::Year;
 
 pub fn main(q: Quarter, y: Year, d: Vec<FilingMetadata>) {
     let conn = get_connection();
-    persist_companies(&conn, &q, &y, d);
+    persist(&conn, &q, &y, d);
 }
 
 /// Get the database connection
@@ -17,13 +17,13 @@ fn get_connection() -> Connection {
     Connection::connect(db_uri, TlsMode::None).expect("Unable to connect to database!")
 }
 
-fn persist_companies(conn: &Connection, q: &Quarter, y: &Year, d: Vec<FilingMetadata>) -> () {
+fn persist(conn: &Connection, q: &Quarter, y: &Year, d: Vec<FilingMetadata>) -> () {
     let q_as_num = *q as i32;
     let y_as_num = *y as i32;
 
     let trans = conn.transaction().expect("Couldn't begin transaction");
 
-    let stmt = trans
+    let company_upsert_stmt = trans
         .prepare(
             "
              INSERT INTO company
@@ -32,9 +32,50 @@ fn persist_companies(conn: &Connection, q: &Quarter, y: &Year, d: Vec<FilingMeta
              ON CONFLICT (short_cik) DO NOTHING;
              ",
         )
-        .expect("Couldn't prepare statement for execution");
-    for each in d {
-        stmt.execute(&[&each.short_cik, &each.company_name])
+        .expect("Couldn't prepare company upsert statement for execution");
+
+    for each in &d {
+        company_upsert_stmt
+            .execute(&[&each.short_cik, &each.company_name])
+            .expect("Couldn't execute update");
+    }
+
+    let filing_type_upsert_stmt = trans
+        .prepare(
+            "
+             INSERT INTO filing_type
+             (filing_name)
+             VALUES ($1)
+             ON CONFLICT DO NOTHING;
+             ",
+        )
+        .expect("Couldn't prepare filing type upsert statement for execution");
+
+    for each in &d {
+        filing_type_upsert_stmt
+            .execute(&[&each.form_type])
+            .expect("Couldn't execute update");
+    }
+
+    let filing_upsert_stmt = trans
+        .prepare(
+            "
+             INSERT INTO filing
+             (company_short_cik, filing_name, filing_edgar_url, filing_quarter, filing_year)
+             VALUES ($1, $2, $3, $4, $5);
+             ",
+        )
+        .expect("Couldn't prepare filing upsert statement for execution");
+
+    for each in &d {
+        filing_upsert_stmt
+            .execute(&[
+                &each.short_cik,
+                &each.form_type,
+                &each.filename,
+                &q_as_num,
+                &y_as_num,
+            ])
             .expect("Couldn't execute update");
     }
 
