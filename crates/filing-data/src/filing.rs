852eb2a9eaf3edb258d3b45e6440909102449259
Marty Jones <murtin.jones@gmail.com>
2019-05-23 08:54:57 -0400

    years and quarters as ints not enums

--- crates/filing-data/src/filing.rs
+++ crates/filing-data/src/filing.rs
@@ -3,10 +3,10 @@ use filing_metadata_lib::time_periods::Year;
 
 #[derive(Debug, FromSql)]
 pub struct Filing {
-    pub id: u32,
+    pub id: i32,
     pub company_short_cik: String,
     pub filing_name: String,
     pub filing_edgar_url: String,
-    pub filing_quarter: Quarter,
-    pub filing_year: Year,
+    pub filing_quarter: i32,
+    pub filing_year: i32,
 }
