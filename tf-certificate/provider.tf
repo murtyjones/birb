7f18b48edc16acbc670e5ec116245527f1df4b1e
Marty Jones <murtin.jones@gmail.com>
2019-05-26 18:41:00 -0400

    delete not-so-useful folders

--- tf-certificate/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  version                 = ">= 1.33.0"
-  shared_credentials_file = "$HOME/.aws/credentials"
-  profile                 = "default"
-  region                  = "${var.aws_region}"
-}
--- tf-certificate/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "domain" {
-  description = "Which domain to use."
-  default     = "birb.io"
-}
-
-variable "aws_region" {
-  description = "The AWS region things are created in"
-  default     = "us-east-1"
-}
