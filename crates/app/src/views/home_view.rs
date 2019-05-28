40f6fd935d924b87f759cd092fdd7be0940313fb
Marty Jones <murtin.jones@gmail.com>
2019-05-28 08:07:16 -0400

    fix store movement issue

--- crates/app/src/views/home_view.rs
+++ crates/app/src/views/home_view.rs
@@ -26,6 +26,9 @@ impl View for HomeView {
         let nav_bar = NavBarView::new(ActivePage::Home).render();
 
         let store = Rc::clone(&self.store);
+        // To avoid moving store into the closure:
+        // More details: https://github.com/rustwasm/wasm-bindgen/issues/1269
+        let store_ref = Some(store);
 
         let click_count = self.store.borrow().click_count();
         let click_count = &*click_count.to_string();
@@ -65,8 +68,9 @@ impl View for HomeView {
                                 .dyn_into::<web_sys::HtmlInputElement>()
                                 .unwrap()
                                 .value();
-                            // TODO figure out how to actually call this:
-                            // download_autocomplete_json(value, store);
+                            // TODO figure out how to actually call this
+                            // Current error: expected an `FnMut<(web_sys::Event,)>` closure, found `[closure@crates/app/src/views/home_view.rs:58:31: 79:24 autocomplete_timeout:std::option::Option<i32>, store_ref:std::option::Option<std::rc::Rc<std::cell::RefCell<store::Store>>>]`
+                            // download_autocomplete_json(value, store_ref.take().unwrap());
                         }) as Box<FnMut(_)>);
                         autocomplete_timeout = Some(web_sys::window().unwrap().set_timeout_with_callback_and_timeout_and_arguments_0(
                             debounced_request.as_ref().unchecked_ref(),
