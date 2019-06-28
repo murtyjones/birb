88dfccfffacff2c4703e93d80fc250b07ab7a659
Marty Jones <murtin.jones@gmail.com>
2019-06-28 08:21:47 -0400

    only download if path doesnt exist

--- crates/filing-parser/src/bin/download_examples.rs
+++ crates/filing-parser/src/bin/download_examples.rs
@@ -2,8 +2,13 @@ extern crate filing_parser;
 
 use aws::s3;
 use filing_parser::test_files::FILES;
-use std::fs::File;
+use std::fs::{metadata, File};
 use std::io::prelude::*;
+use std::path::Path;
+
+pub fn path_exists(file_path: &String) -> bool {
+    Path::new(file_path).exists()
+}
 
 fn write_to_file(file_path: &String, data: Vec<u8>) -> std::io::Result<()> {
     let mut pos = 0;
@@ -16,11 +21,14 @@ fn write_to_file(file_path: &String, data: Vec<u8>) -> std::io::Result<()> {
     Ok(())
 }
 
+/// Downloads any filings that are missing from the examples needed for testing
 fn main() {
     let client = s3::get_s3_client();
     for i in 0..FILES.len() {
         let file = &FILES[i];
-        let data = s3::get_s3_object(&client, "birb-edgar-filings", file.s3.as_str());
-        write_to_file(&file.path, data);
+        if !path_exists(&file.path) {
+            let data = s3::get_s3_object(&client, "birb-edgar-filings", file.s3.as_str());
+            write_to_file(&file.path, data);
+        }
     }
 }
