6f6f58e27ba2d3b75a8a4dc8cff138018adeab98
Marty Jones <murtin.jones@gmail.com>
2019-07-23 07:27:11 -0400

    remove todos

--- js/app/reducers/state.ts
+++ js/app/reducers/state.ts
@@ -1,14 +1,12 @@
-import { TodoModel, CompanyModel } from 'app/models';
+import { CompanyModel } from 'app/models';
 
 export interface RootState {
-  todos: RootState.TodoState;
   companies: RootState.CompanyState;
   loading: RootState.LoadingState;
   router?: any;
 }
 
 export namespace RootState {
-  export type TodoState = TodoModel[];
   export type CompanyState = { byShortCik: Record<CompanyModel['shortCik'], CompanyModel> };
   export type LoadingState = { [requestName: string]: boolean };
 }
