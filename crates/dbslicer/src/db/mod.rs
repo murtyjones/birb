5f36633aaf8f9af141622b313702f9eaf78a7c73
Marty Jones <murtin.jones@gmail.com>
2019-07-30 21:12:47 -0500

    .

--- crates/dbslicer/src/db/mod.rs
+++ crates/dbslicer/src/db/mod.rs
@@ -16,35 +16,12 @@ pub fn get_companies(conn: &Connection) -> Rows {
     ).expect("Couldn't get companies")
 }
 
-fn get_co_model(companies: &Rows) -> Vec<Company> {
-    let mut binds = Vec::new();
-    for row in companies {
-        binds.push(Company {
-            short_cik: row.get("short_cik"),
-            company_name: row.get("company_name"),
-        });
-    }
-    binds
-}
-
-pub fn get_filings(conn: &Connection, companies: &Rows) -> Rows {
-    let companies = get_co_model(companies);
-    let short_ciks = companies.iter().map(|c: &Company|
-        c.short_cik
-    );
+pub fn get_company_filings(conn: &Connection, company: &Company) -> Rows {
     conn.query(
         r#"
-            SELECT * FROM company
-            ORDER BY created_at ASC
+            SELECT * FROM filing
+            WHERE short_cik = $1
             LIMIT 100000
-        "#, &[]
-    ).expect("Couldn't get companies")
-}
-
-pub fn get_filing_types(conn: &Connection) -> Rows {
-    conn.query(
-        r#"
-            SELECT * FROM filing_type
-        "#, &[]
+        "#, &[&company.short_cik]
     ).expect("Couldn't get companies")
 }
--- crates/dbslicer/src/main.rs
+++ crates/dbslicer/src/main.rs
@@ -1,8 +1,10 @@
 pub mod db;
 
 use aws;
-use db::*;
 use std::thread::sleep;
+use models::Company;
+use db::*;
+use rayon::prelude::*;
 
 pub fn get_prod_conn_string() -> String {
     let port = std::str::from_utf8(include_bytes!("../../../scripts/local_port")).unwrap().to_string();
@@ -17,11 +19,15 @@ fn main() {
 //    sleep(std::time::Duration::from_secs(3));
     let prod_conn_string = get_prod_conn_string();
     let production_connection = get_connection(prod_conn_string);
-    println!("PROD CONN {:?}", production_connection);
     let local_connection = get_connection("postgres://postgres:develop@localhost:5432/postgres");
-    println!("LOCAL CONN {:?}", local_connection);
     let companies = get_companies(&production_connection);
-    let filing_types = get_filing_types(&production_connection);
-    let filings = get_filings(&production_connection, &companies);
-    for row in companies.iter() {}
+    let company_filings = get_filings(&production_connection, &companies);
+    for row in companies.par_iter() {
+        let company = Company {
+            short_cik: row.get("short_cik"),
+            company_name: row.get("company_name"),
+        };
+        let company_filings = get_company_filings(&production_connection, &company);
+        // https://github.com/sfackler/rust-postgres#statement-preparation
+    }
 }
