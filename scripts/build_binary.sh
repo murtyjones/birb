770d4a2c67d1a48e990111245b622116fb91dace
Marty Jones <murtin.jones@gmail.com>
2019-08-12 21:21:19 -0400

    try compiling a different way

--- scripts/build_binary.sh
+++ scripts/build_binary.sh
@@ -9,15 +9,15 @@ PACKAGE=$1
 # Go to project root
 cd $(git rev-parse --show-toplevel)
 
-# get base image
-docker pull clux/muslrust:nightly
-
-# build binary
-docker run --rm \
-    -v cargo-cache:/usr/local/cargo \
-    -v target-cache:$PWD/target \
-    -v mac-cargo-cache:$HOME/.cargo/registry \
-    -v $PWD:/volume \
-    -w /volume \
-    -it clux/muslrust:nightly \
-    cargo build -p ${PACKAGE} --release
+## get base image
+#docker pull clux/muslrust:nightly
+#
+## build binary
+#docker run --rm \
+#    -v cargo-cache:/usr/local/cargo \
+#    -v target-cache:$PWD/target \
+#    -v mac-cargo-cache:$HOME/.cargo/registry \
+#    -v $PWD:/volume \
+#    -w /volume \
+#    -it clux/muslrust:nightly \
+    cargo build -p ${PACKAGE} --release --target=x86_64-unknown-linux-gnu
