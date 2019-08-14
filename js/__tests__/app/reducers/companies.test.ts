16dc41a89dfab7f942f83e2beb9b568e29bf2e30
Marty Jones <murtin.jones@gmail.com>
2019-08-13 21:42:27 -0400

    fix test

--- js/__tests__/app/reducers/companies.test.ts
+++ js/__tests__/app/reducers/companies.test.ts
@@ -1,13 +1,13 @@
 const { shallow } = require('enzyme');
-import { companyReducer } from 'app/reducers/companies';
 import { CompanyActions } from 'app/actions/companies';
+import { companyReducer } from 'app/reducers/companies';
 
 describe('Companies Reducer', () => {
     it('Adds a company to empty state on successful retrieval', () => {
         // Arrange
         const action = {
+            payload: { shortCik: '123', name: 'Tezzla', filings: [] },
             type: CompanyActions.Type.GET_COMPANY_SUCCESS as CompanyActions.Type.GET_COMPANY_SUCCESS,
-            payload: { shortCik: '123', name: 'Tezzla', filings: [] }
         };
 
         // Act
@@ -17,21 +17,21 @@ describe('Companies Reducer', () => {
         // Assert
         expect(newState).toEqual({
             byShortCik: {
-                '123': { shortCik: '123', name: 'Tezzla', filings: [] }
-            }
-        })
+                123: { shortCik: '123', name: 'Tezzla', filings: [] },
+            },
+        });
     });
 
     it('Adds a company to existing state on successful retrieval', () => {
         // Arrange
         const initialState = {
             byShortCik: {
-                '000': { shortCik: '000', name: 'NotTezzla', filings: [] }
-            }
+                '000': { shortCik: '000', name: 'NotTezzla', filings: [] },
+            },
         };
         const action = {
+            payload: { shortCik: '123', name: 'Tezzla', filings: [] },
             type: CompanyActions.Type.GET_COMPANY_SUCCESS as CompanyActions.Type.GET_COMPANY_SUCCESS,
-            payload: { shortCik: '123', name: 'Tezzla', filings: [] }
         };
 
         // Act
@@ -41,9 +41,17 @@ describe('Companies Reducer', () => {
         // Assert
         expect(newState).toEqual({
             byShortCik: {
-                '000': { shortCik: '000', name: 'NotTezzla' },
-                '123': { shortCik: '123', name: 'Tezzla', filings: [] }
-            }
-        })
+                '000': {
+                    filings: [],
+                    name: 'NotTezzla',
+                    shortCik: '000',
+                },
+                '123': {
+                    filings: [],
+                    name: 'Tezzla',
+                    shortCik: '123',
+                },
+            },
+        });
     });
 });
