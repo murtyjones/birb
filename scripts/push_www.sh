2c8488c02b3e23259871e0f9ddbd47b1b03372fc
Marty Jones <murtin.jones@gmail.com>
2019-08-14 03:27:41 -0400

    split build and deploy steps for app

--- scripts/push_www.sh
+++ scripts/push_www.sh
@@ -6,13 +6,11 @@ set -e
 # Go to project root
 cd $(git rev-parse --show-toplevel)
 
-# Build
-npm run build
-
 # Upload to S3
 aws s3 sync ./build s3://birb.io/
 
-cloudfront_id=$(<terraform/out/www_cloudfront_id)
+#cloudfront_id=$(<terraform/out/www_cloudfront_id)
+cloudfront_id=E37JWK8PNPGTX7
 
 # Invalidate cache
 aws cloudfront create-invalidation --distribution-id ${cloudfront_id} --paths /index.html
