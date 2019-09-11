bfb992cfd00b5cfae6176f180b157f6c2963ab2b
Marty Jones <murtin.jones@gmail.com>
2019-09-11 07:48:18 -0400

    fix compile error

--- crates/filing-parser/src/bin/download_examples.rs
+++ crates/filing-parser/src/bin/download_examples.rs
@@ -6,7 +6,7 @@ use filing_parser::helpers::path_exists;
 use filing_parser::test_files::get_files;
 use flate2::read::GzDecoder;
 use tendril::fmt::Slice;
-use utils::write_to_file;
+use utils::{decompress_gzip, write_to_file};
 
 /// Downloads any filings that are missing from the examples needed for testing
 fn main() {
@@ -16,14 +16,11 @@ fn main() {
         let path = String::from(file.path);
         if !path_exists(&path) {
             let data = s3::get_s3_object(&client, "birb-edgar-filings", file.s3);
-            let mut d = GzDecoder::new(data.as_bytes());
-            let mut decompressed = String::new();
-            d.read_to_string(&mut decompressed).unwrap();
+            let decompressed_data = decompress_gzip(data);
 
-            println!("{}", data.len());
-            write_to_file(&path, ".txt", decompressed.clone().into_bytes())
+            write_to_file(&path, ".txt", decompressed_data.clone().into_bytes())
                 .expect("Couldn't write to file!");
-            write_to_file(&path, ".html", decompressed.clone().into_bytes())
+            write_to_file(&path, ".html", decompressed_data.clone().into_bytes())
                 .expect("Couldn't write to file!");
         }
     }
